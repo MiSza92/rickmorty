9bd5942708aef1cd5a94206f6269661ec95b80de
MSZ <m.szabo@outlook.de>
2024-02-21 18:30:01 +0100

    Aktualisiere Pfade für GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://synav92.github.io/rickmorty/",
+  base: "/rickmorty/",
 });
